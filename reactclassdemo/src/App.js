f3b6c48e00ea76b04b64cad4d69f98ff5af357e7
hariprasad154 <hariprasas154@gmail.com>
2023-05-22 00:07:35 +0530

    added

--- reactclassdemo/src/App.js
+++ reactclassdemo/src/App.js
@@ -2,17 +2,33 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 class App extends React.Component{
+  url='https://www.reactjs.com/'
   constructor(){
   super()
   this.state={
-    title:'hello from bridgelabs '
+    title:'hello from bridgelabs ',
+    userName:''
   }
 }
+onClick=($event)=>{
+  window.open(this.url,"_blank");
+}
+onNameChange=(event)=>{
+  this.setState({userName: event.target.value});
+
+}
 render(){
-  return(
+  return(<>
     <div>
-      <h1>{this.state.title}</h1>
+      <h1>{this.state.title} {this.state.userName}</h1>
+      <img src={logo} onClick={this.onClick}
+      alt="The bridgeLab logi :a bridge to Employee"/>
+
     </div>
+    <div>
+      <input onChange={this.onNameChange}/>
+    </div>
+    </>
   )
 }
 }
