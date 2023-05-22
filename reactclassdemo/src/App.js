5e02abdca0362aa26632f0c876685567befe2ea2
hariprasad154 <hariprasas154@gmail.com>
2023-05-22 18:12:11 +0530

    added

--- reactclassdemo/src/App.js
+++ reactclassdemo/src/App.js
@@ -1,46 +1,12 @@
-import logo from './logo.svg';
 import React from 'react';
+import Home from'./component/Home.js';
 import './App.css';
 class App extends React.Component{
-  url='https://www.reactjs.com/'
-  constructor(){
-  super()
-  this.state={
-    title:'hello from bridgelabs ',
-    userName:'',
-    nameError:' '
-  }
-}
-onClick=($event)=>{
-  console.log("save botton ",$event)
-  window.open(this.url,"_blank");
-}
-onNameChange=(event)=>{
-  console.log(" the name ",event.target.value);
-  const nameRegex=RegExp('^[A-Z]{1}[a-z]{2,}$');
-
-  if(nameRegex.test(event.target.value)){
-    this.setState({userName: event.target.value});
-    this.setState({nameError:''})
-  }else{
-    this.setState({nameError:'Name is not correct Starts With Capital and min 3 char'})
-  }
-}
-render(){
+ render(){
   return(<div>
-    <div>
-      <h1>{this.state.title} {this.state.userName}</h1>
-      <img src={logo} onClick={this.onClick}
-      alt="The bridgeLab logo :a bridge to Employee"/>
-
-    </div>
-    <div>
-      <input className="nameInput" onChange={this.onNameChange}/>
-      <span className="error-output">{this.state.nameError}</span>
-    </div>
-    </div>
-  )
+    {/* App Component */}
+    <Home/>
+  </div>);
 }
 }
-
 export default App;
